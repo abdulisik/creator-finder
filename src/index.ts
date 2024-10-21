de6a9ac547e8828c3e40f2dde3abf0182c39aa4d
Abdul Isik <aisik@dotdash.com>
2024-10-21 00:26:49 -0500

    Add error handling and update query

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
 type Bindings = {
-  DB: D1Database
-}
+  DB: D1Database;
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/all', async (c) => {
-  const results = await c.env.DB.prepare("SELECT creator.id, creator.link, youtube.handle FROM creator JOIN youtube ON creator.youtube = youtube.id;").all();
-  return c.json(results.results);
-})
+app.get("/all", async (c) => {
+  try {
+    const results = await c.env.DB.prepare(
+      "SELECT creator.link, youtube.handle FROM creator JOIN youtube ON creator.youtube = youtube.id;"
+    ).all();
+    return c.json(results.results);
+  } catch (e) {
+    console.error(e);
+    return c.json(
+      { error: e instanceof Error ? e.message : `Unknown error: ${e}` },
+      500
+    );
+  }
+});
 
-export default app
+export default app;
